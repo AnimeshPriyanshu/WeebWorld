08d546f025070ae5ce39623205a39086994d9b8d
AnimeshPriyanshu <priyanshuanimesh@gmail.com>
2026-04-01 22:48:40 +0530

    added list from API

--- apicheck.js
+++
@@ -1,8 +0,0 @@
-let query = "naruto"
-async function fetchData(query) {
-    let res = await fetch(`https://api.jikan.moe/v4/anime?q=${query}`);
-let data = await res.json();
-
-console.log(data.images); // full object
-}
-fetchData(query)
