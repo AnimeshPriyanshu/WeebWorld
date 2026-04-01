81dd85861a65dc93eba122564816bbb39087c46b
AnimeshPriyanshu <priyanshuanimesh@gmail.com>
2026-04-01 22:48:16 +0530

    added list from API

---
+++ index.js
@@ -0,0 +1,33 @@
+document.getElementById("btn").addEventListener("click", searchAnime);
+
+document.getElementById("search").addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        searchAnime();
+    }
+});
+
+async function searchAnime() {
+    let query = document.getElementById("search").value;
+
+    if (query === "") return;
+
+    let res = await fetch(`https://api.jikan.moe/v4/anime?q=${query}&limit=6`);
+    let data = await res.json();
+
+    let results = document.getElementById("results");
+    results.innerHTML = "";
+
+    for (let i = 0; i < data.data.length; i++) {
+        let anime = data.data[i];
+
+        let div = document.createElement("div");
+        div.className = "card";
+
+        div.innerHTML = `
+            <img src="${anime.images.jpg.image_url}">
+            <p>${anime.title}</p>
+        `;
+
+        results.appendChild(div);
+    }
+}
