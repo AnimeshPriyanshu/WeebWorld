ce132af5955ba25de5f1ee0ffa02abdf9f5d9790
AnimeshPriyanshu <priyanshuanimesh@gmail.com>
2026-04-07 17:25:23 +0530

    fixed: anime poster

--- index.js
+++ index.js
@@ -9,18 +9,20 @@ document.getElementById("search").addEventListener("keydown", function (e) {
 async function searchAnime() {
     let query = document.getElementById("search").value
     if (query === "") return
-    let res = await fetch(`https://api.jikan.moe/v4/anime?q=${query}`)
+
+    let res = await fetch("https://api.jikan.moe/v4/anime?q=" + query)
     let data = await res.json()
     let results = document.getElementById("results")
     results.innerHTML = ""
+
     for (let i = 0; i < data.data.length; i++) {
         let anime = data.data[i]
         let div = document.createElement("div")
         div.className = "card"
         div.innerHTML = `
-            <img src="${anime.images.jpg.image_url}">
+            <img src="${anime.images.jpg.image_url}" alt="${anime.title}">
             <p>${anime.title}</p>
         `
-        results.appendChild(div);
+        results.appendChild(div)
     }
 }
